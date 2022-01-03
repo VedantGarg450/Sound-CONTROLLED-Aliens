5035c71a250c1659a5d41e8b12b940b2918f89d2
VedantGarg450 <86777859+VedantGarg450@users.noreply.github.com>
2022-01-03 16:40:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,35 @@ function GotResult(error, result){
     }
     else{
         console.log(result)
+        document.getElementById("noise").innerHTML= result[0].label;
+        document.getElementById("confidence").innerHTML= (result[0].confidence*100).toFixed(2)+"%";
+        if(result[0].label=="Background Noise"){
+            document.getElementById("A1").src="aliens-01.gif"
+            document.getElementById("A2").src="aliens-02.png"
+            document.getElementById("A3").src="aliens-03.png"
+            document.getElementById("A4").src="aliens-04.png"
+        }
+        else if(result[0].label=="Clapping"){
+            document.getElementById("A1").src="aliens-01.png"
+            document.getElementById("A2").src="aliens-02.gif"
+            document.getElementById("A3").src="aliens-03.png"
+            document.getElementById("A4").src="aliens-04.png"
+        }
+        else if(result[0].label=="Drumming/Banging"){
+            document.getElementById("A1").src="aliens-01.png"
+            document.getElementById("A2").src="aliens-02.png"
+            document.getElementById("A3").src="aliens-03.gif"
+            document.getElementById("A4").src="aliens-04.png"
+        }
+        else if(result[0].label=="Whistling"){
+            document.getElementById("A1").src="aliens-01.png"
+            document.getElementById("A2").src="aliens-02.png"
+            document.getElementById("A3").src="aliens-03.png"
+            document.getElementById("A4").src="aliens-04.gif"
+        }
+
+
+
     }
 
 }
